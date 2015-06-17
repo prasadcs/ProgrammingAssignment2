32ef03c252355da1d74ec82736c5f7c8f3e420f6
prasadcs <prasad.senguttuvan@gmail.com>
2015-06-17 15:29:22 +0530

    upload progassmnt2 file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## function makeCacheMatrix creates a list of functions 
+## to be used by function cacheSolve 
 
-## Write a short comment describing this function
+## function makeCacheMatrix to create a list of functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) i <<- inv
+        getinv <- function() i
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## function cacheSolve to get the inverse of a matrix 
+## either by calculating or from cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinv()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinv(i)
+        i
 }
